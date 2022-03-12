f346a5775fa53d9a66b8534fd2d99be8495b0f2e
Andrea Verlicchi <andrea.verlicchi@netcentric.biz>
2022-03-12 17:59:07 +0100

    Added shared formulas in Excel

--- bin/excel.js
+++ bin/excel.js
@@ -1,13 +1,26 @@
 import ExcelJS from "exceljs";
 
-const wb = new ExcelJS.Workbook();
-const ws = wb.addWorksheet("Oh, sheet");
+const workbook = new ExcelJS.Workbook();
+const worksheet = workbook.addWorksheet("Oh, sheet");
 
-ws.getCell("A1").value = 2;
+/* ws.getCell("A1").value = 2;
 ws.getCell("B1").value = 3;
 ws.getCell("C1").value = { formula: "A1 / B1" };
+ */
+worksheet.columns = [
+  { header: "Id", key: "id" },
+  { header: "Name", key: "name" },
+  { header: "Age", key: "age" },
+  { header: "Average", key: "average" },
+];
 
-wb.xlsx
+worksheet.addRow({ id: 1, name: "John Doe", age: 35 });
+worksheet.addRow({ id: 2, name: "ORgo Ciald", age: 12 });
+worksheet.addRow({ id: 3, name: "Ciadk voj", age: 34 });
+worksheet.addRow({ id: 4, name: "Diavolo a 4", age: 69 });
+worksheet.fillFormula("D2:D5", "C2 * A2");
+
+workbook.xlsx
   .writeFile("./data/extracted.xlsx")
   .then(() => {
     console.log("Done.");
