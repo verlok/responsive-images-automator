b052f1b4ba052ab9749977f7c1fa80c1511a2c89
Andrea Verlicchi <andrea.verlicchi@netcentric.biz>
2022-03-19 10:24:51 +0100

    Using pixelRatio from constants

--- bin/lib/calcImgColumns.js
+++ bin/lib/calcImgColumns.js
@@ -1,7 +1,7 @@
-import { VIEWPORT_WIDTH } from "./constants.js";
+import { PIXEL_RATIO, VIEWPORT_WIDTH } from "./constants.js";
 
 export function calcIdealIntrinsicWidth(imgWidth, resolution, fidelityCap) {
-  return Math.round(imgWidth * Math.min(resolution.pixelRatio, fidelityCap));
+  return Math.round(imgWidth * Math.min(resolution[PIXEL_RATIO], fidelityCap));
 }
 
 export function calcImgVW(imgWidth, resolution) {
