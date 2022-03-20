2bc05ad82ee8bdb3949251a27faf46eb3c0871a8
Andrea Verlicchi <andrea.verlicchi@netcentric.biz>
2022-03-20 20:56:07 +0100

    Testing change url to webpagetest image

--- bin/test-probe.js
+++ bin/test-probe.js
@@ -1,8 +1,9 @@
 import probe from "probe-image-size";
 //const imgUrl = "https://via.placeholder.com/350x150";
 //const imgUrl = "https://www.dunhill.com/product_image/45634378WS/f/w508_bffffff.jpg";
-const imgUrl = "https://www.henkel.com/resource/image/1610198/16x9/1920/1098/803be6c94a6adfefc53bea6350229de2/DG/laboratory-4-zu-3.jpg";
+//const imgUrl = "https://www.henkel.com/resource/image/1610198/16x9/1920/1098/803be6c94a6adfefc53bea6350229de2/DG/laboratory-4-zu-3.jpg";
 //const imgUrl = "https://2022.cssday.it/img/speakers/massimo-artizzu.png";
+const imgUrl = "https://webpagetest.org/images/wpt_home_featureimg.jpg";
 //let result = await probe(imgUrl);
 //console.log(result);
 
