3b83f07732ff32139feb58575e95fe71f48974b4
Andrea Verlicchi <andrea.verlicchi@netcentric.biz>
2022-03-21 22:32:22 +0100

    Tests green for web.dev

--- __tests__/webdev-mainline.test.js
+++ __tests__/webdev-mainline.test.js
@@ -1,21 +1,21 @@
-const pageName = "home";
+const pageName = "webdev-mainline";
 const testTable = [
-  { viewportWidth: 375, pixelRatio: 3, expectedIntrinsicWidth: 708 },
-  { viewportWidth: 414, pixelRatio: 2, expectedIntrinsicWidth: 708 },
-  { viewportWidth: 390, pixelRatio: 3, expectedIntrinsicWidth: 708 },
-  { viewportWidth: 375, pixelRatio: 2, expectedIntrinsicWidth: 708 },
-  { viewportWidth: 414, pixelRatio: 3, expectedIntrinsicWidth: 708 },
-  { viewportWidth: 360, pixelRatio: 3, expectedIntrinsicWidth: 708 },
-  { viewportWidth: 428, pixelRatio: 3, expectedIntrinsicWidth: 708 },
-  { viewportWidth: 1920, pixelRatio: 1, expectedIntrinsicWidth: 560 },
-  { viewportWidth: 412, pixelRatio: 2.63, expectedIntrinsicWidth: 708 },
-  { viewportWidth: 1440, pixelRatio: 2, expectedIntrinsicWidth: 1076 },
-  { viewportWidth: 1366, pixelRatio: 1, expectedIntrinsicWidth: 560 },
-  { viewportWidth: 360, pixelRatio: 2, expectedIntrinsicWidth: 708 },
-  { viewportWidth: 768, pixelRatio: 2, expectedIntrinsicWidth: 560 },
-  { viewportWidth: 393, pixelRatio: 2.75, expectedIntrinsicWidth: 708 },
-  { viewportWidth: 1536, pixelRatio: 1.25, expectedIntrinsicWidth: 708 },
-  { viewportWidth: 320, pixelRatio: 2, expectedIntrinsicWidth: 560 },
+  { viewportWidth: 375, pixelRatio: 3, expectedIntrinsicWidth: 1125 },
+  { viewportWidth: 414, pixelRatio: 2, expectedIntrinsicWidth: 828 },
+  { viewportWidth: 390, pixelRatio: 3, expectedIntrinsicWidth: 1125 },
+  { viewportWidth: 375, pixelRatio: 2, expectedIntrinsicWidth: 750 },
+  { viewportWidth: 414, pixelRatio: 3, expectedIntrinsicWidth: 1366 },
+  { viewportWidth: 360, pixelRatio: 3, expectedIntrinsicWidth: 1125 },
+  { viewportWidth: 428, pixelRatio: 3, expectedIntrinsicWidth: 1366 },
+  { viewportWidth: 1920, pixelRatio: 1, expectedIntrinsicWidth: 1600 },
+  { viewportWidth: 412, pixelRatio: 2.63, expectedIntrinsicWidth: 1125 },
+  { viewportWidth: 1440, pixelRatio: 2, expectedIntrinsicWidth: 1920 },
+  { viewportWidth: 1366, pixelRatio: 1, expectedIntrinsicWidth: 1366 },
+  { viewportWidth: 360, pixelRatio: 2, expectedIntrinsicWidth: 750 },
+  { viewportWidth: 768, pixelRatio: 2, expectedIntrinsicWidth: 1600 },
+  { viewportWidth: 393, pixelRatio: 2.75, expectedIntrinsicWidth: 1125 },
+  { viewportWidth: 1536, pixelRatio: 1.25, expectedIntrinsicWidth: 1920 },
+  { viewportWidth: 320, pixelRatio: 2, expectedIntrinsicWidth: 750 },
 ];
 
 // -------------
