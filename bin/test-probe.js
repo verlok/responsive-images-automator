134bbcd09b7f2fda6ad77c2d55ae08975d52bfaf
Andrea Verlicchi <andrea.verlicchi@netcentric.biz>
2022-03-21 06:05:36 +0100

    Extraction points to Schwarzkopf.de

--- bin/test-probe.js
+++ bin/test-probe.js
@@ -3,7 +3,8 @@ import probe from "probe-image-size";
 //const imgUrl = "https://www.dunhill.com/product_image/45634378WS/f/w508_bffffff.jpg";
 //const imgUrl = "https://www.henkel.com/resource/image/1610198/16x9/1920/1098/803be6c94a6adfefc53bea6350229de2/DG/laboratory-4-zu-3.jpg";
 //const imgUrl = "https://2022.cssday.it/img/speakers/massimo-artizzu.png";
-const imgUrl = "https://webpagetest.org/images/wpt_home_featureimg.jpg";
+//const imgUrl = "https://webpagetest.org/images/wpt_home_featureimg.jpg";
+const imgUrl = "https://dm.henkel-dam.com/is/image/henkel/Taft_campaign_2022_merkel_Magenta_Banner?wid=2560&fit=hfit&qlt=60";
 //let result = await probe(imgUrl);
 //console.log(result);
 
