bcdccc2b8e0f30a6e18133e62f77eb03bcdc1e91
Andrea Verlicchi <andrea.verlicchi@netcentric.biz>
2022-04-23 00:59:47 +0200

    Forcing reload to avoid pages stuck on cookie layer

--- bin/lib/extractPageData.js
+++ bin/lib/extractPageData.js
@@ -6,19 +6,20 @@ import {
   IMG_WIDTH,
   IMG_VW,
   IDEAL_INTRINSIC_WIDTH,
-  USAGE,
   IMAGE_CSS_SELECTOR,
   CURRENT_INTRINSIC_WIDTH,
 } from "./constants.js";
 
 export default async function (resolutions, page, extraction, fidelityCap) {
   const currentPageData = [];
+  const forceReload = true; 
   for (const resolution of resolutions) {
     const imageCssSelector = extraction[IMAGE_CSS_SELECTOR];
     const { imgWidth, imgIntrinsicWidth } = await getImageWidthAtViewport(
       page,
       resolution,
-      imageCssSelector
+      imageCssSelector,
+      forceReload
     );
     //await takeScreenshot(page, resolution, extraction[PAGE_NAME]);
     currentPageData.push({
