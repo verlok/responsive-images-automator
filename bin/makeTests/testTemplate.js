47b46a26ed7d696c93d3c25187234fbf86747029
Andrea Verlicchi <andrea.verlicchi@netcentric.biz>
2022-04-23 23:25:47 +0200

    refactored testTemplate

--- bin/makeTests/testTemplate.js
+++ bin/makeTests/testTemplate.js
@@ -1,6 +1,13 @@
-export default (
+const getTestCasesRows = (testCases) =>
   testCases
-) => `const testFnFactory = require("./factory/testFnFactory");
+    .map(
+      ({ viewportWidth, pixelRatio, chosenIntrinsicWidth }) =>
+        `\${${viewportWidth}} | \${${pixelRatio}} | \${${chosenIntrinsicWidth}}\n`
+    )
+    .join("");
+
+export default (testCases) => `
+const testFnFactory = require("./factory/testFnFactory");
 
 const pageName = "webdev-home";
 const pageUrl = \`http://localhost:8080/page/\${pageName}\`;
@@ -8,10 +15,7 @@ const pageUrl = \`http://localhost:8080/page/\${pageName}\`;
 describe(\`Testing \${pageName} page image\`, () => {
   test.each\`
     viewportWidth | pixelRatio | expectedIntrinsicWidth
-    ${testCases.map(
-      ({ viewportWidth, pixelRatio, chosenIntrinsicWidth }) => 
-      `\${${viewportWidth}} | \${${pixelRatio}} | \${${chosenIntrinsicWidth}}\n`
-    ).join('')}\`(
+    ${getTestCasesRows(testCases)}\`(
     \`When viewport is $viewportWidth @ $pixelRatio, image intrinsic width should be $expectedIntrinsicWidth\`,
     testFnFactory(pageUrl)
   );
